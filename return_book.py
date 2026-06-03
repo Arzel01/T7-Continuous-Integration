307aeadb422662c78ade5dff21640674ebcd65b4
stikrobinson <srobinso@espol.edu.ec>
2026-06-03 19:45:58 +0000

    fixture: failed test correction

--- return_book.py
+++ return_book.py
@@ -42,11 +42,12 @@ def return_book(member_id, book_id):
     """
     with open("data.json", "r", encoding="utf-8") as file:
         loaded_data = json.load(file)
-        for loan in loaded_data["loans"]:
-            if loan["book_isbn"] == book_id and loan["member_id"] == member_id:
-                loaded_data["loans"].remove(loan)
-                with open("data.json", "w", encoding="utf-8") as file:
-                    json.dump(loaded_data, file, indent=4, encoding="utf-8")
-                return loaded_data
+        
+    for loan in loaded_data["loans"]:
+        if loan["book_isbn"] == book_id and loan["member_id"] == member_id:
+            loaded_data["loans"].remove(loan)
+            with open("data.json", "w", encoding="utf-8") as file:
+                json.dump(loaded_data, file, indent=4)
+            return loaded_data
     loaded_data = {"Error": "Loan does not exist"}
     return loaded_data
